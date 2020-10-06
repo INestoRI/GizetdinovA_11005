b895a12ae9dbd24fe2186f287bf1b3fb812044dd
Albert Gizetdinov <albert160616@gmail.com>
2020-10-06 14:16:35 +0300

    newCommit

---
+++ FastSorting0910.java
@@ -0,0 +1,8 @@
+//Реализовать быструю сортировку
+import java.util.Scanner;
+public class FastSorting0910 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
