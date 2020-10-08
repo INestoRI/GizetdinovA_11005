b2ccb258183b5939a8015bc70b9e742e9e820dde
Albert Gizetdinov <albert160616@gmail.com>
2020-10-08 20:48:32 +0300

    Homework

--- FastSorting0910.java
+++
@@ -1,8 +0,0 @@
-//Реализовать быструю сортировку
-import java.util.Scanner;
-public class FastSorting0910 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-    }
-}
---
+++ QuickSort0910.java
@@ -0,0 +1,51 @@
+//Реализовать быструю сортировку
+import java.util.Scanner;
+public class QuickSort0910 {
+    public static void quickSort(int[] mas, int x, int y) {
+        if (mas.length == 0)
+            return;
+        if (x >= y)
+            return;
+        int mid1 = x + (y - x) / 2;
+        int mid2 = mas[mid1];
+        int i = x, j = y;
+        while (i <= j) {
+            while (mas[i] < mid2) {
+                i++;
+            }
+            while (mas[j] > mid2) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = mas[i];
+                mas[i] = mas[j];
+                mas[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (x < j)
+            quickSort(mas, x, j);
+        if (y > i)
+            quickSort(mas, i, y);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter massive size: ");
+        int a = in.nextInt();
+
+        int[] mas = {5, 18, 3, 11, 9, 6, 88};
+        System.out.println("Massive before: ");
+        for (int i = 0; i < a; i++) {
+            mas[i] = ((int)(Math.random() * 50));
+            System.out.print(mas[i] + " ");
+        }
+        int low = 0;
+        int high = mas.length - 1;
+        quickSort(mas, low, high);
+        System.out.println("\nMassive after: ");
+        for (int i = 0; i < a; i++) {
+            System.out.print(mas[i] + " ");
+        }
+    }
+}
