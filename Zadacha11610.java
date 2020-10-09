30ff5d214675575abcfc266f12f2f51526fdde8b
Albert Gizetdinov <albert160616@gmail.com>
2020-10-09 11:05:57 +0300

    Homework1610

--- Zadacha11610.java
+++ Zadacha11610.java
@@ -1,6 +1,7 @@
 // Написать метод, который считает количество разрядов числа
 import java.util.Scanner;
 public class Zadacha11610 {
+
     public static int r(int i) {
         int j = 0;
         while (i > 0) {
@@ -9,6 +10,7 @@ public class Zadacha11610 {
         }
         return j;
     }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число: ");
