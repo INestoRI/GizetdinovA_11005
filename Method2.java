fb3a88000b4b2682fa021a17324f08df73a73722
Albert Gizetdinov <albert160616@gmail.com>
2020-10-16 11:26:31 +0300

    Homework2310

--- Method2.java
+++ Method2.java
@@ -1,7 +1,20 @@
 //Рекурсивно вывести целые числа от A до B (A < B)
 import java.util.Scanner;
 public class Method2 {
+
     public static void method(int a, int b) {
-        
+        if (a > b)
+            return;
+        method(a, b - 1);
+        System.out.print(b + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int a = sc.nextInt();
+        System.out.print("Enter number 2: ");
+        int b = sc.nextInt();
+        method(a, b);
     }
 }
