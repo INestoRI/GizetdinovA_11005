6ddf485117d1e3c5640d095c6f5ee79235a32375
Albert Gizetdinov <albert160616@gmail.com>
2020-10-20 12:41:16 +0300

    Homework2310

---
+++ Method4.java
@@ -0,0 +1,21 @@
+//
+import java.util.Scanner;
+public class Method4 {
+
+    public static int func(int m, int n) {
+        if ((n == 0) && (m > 0))
+            return func((m - 1), (1));
+        if ((m > 0) && (n > 0))
+            return func((m - 1), func(m, (n - 1)));
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Result: " + func(a, b));
+    }
+}
