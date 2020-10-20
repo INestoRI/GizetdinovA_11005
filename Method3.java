350247f3a35a7f5f656ed9705143ed8de92a23ff
Albert Gizetdinov <albert160616@gmail.com>
2020-10-20 12:28:27 +0300

    Homework2310

---
+++ Method3.java
@@ -0,0 +1,19 @@
+//Рекурсивно вычислить число Фибоначчи с номером N.
+import java.util.Scanner;
+public class Method3 {
+
+    public static int fib(int n) {
+        if (n == 1)
+            return 0;
+        if (n == 2)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter index of number: ");
+        int a = sc.nextInt();
+        System.out.print("Number with index " + a + " equals: " + fib(a));
+    }
+}
