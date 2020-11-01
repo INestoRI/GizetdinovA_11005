ec871dd1f7738f279bedee03c413615670a09f6b
Albert Gizetdinov <albert160616@gmail.com>
2020-11-01 13:08:46 +0300

    Homework0611

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Vector2D vec = new Vector2D();
+        vec.setVec(3, 8);
+        vec.getVec();
+        vec.setAdd(4, 8);
+        vec.getAdd();
+        vec.setSub(5, 9);
+        vec.getSub();
+        vec.setMult(4);
+        vec.getMult();
+    }
 }
--- Vector2D.java
+++ Vector2D.java
@@ -1,34 +1,36 @@
 public class Vector2D {
     private double x;
     private double y;
-    private double S;
-    private double Add;
-    private double Sub;
-    private double Mult;
-    double a = 3;
-    double b = 2;
-    double c = 4;
 
-    public void vector2D (double x, double y) {
+    public void setVec (double x, double y) {
         this.x = x;
         this.y = y;
     }
-
-    public void vLen(double x, double y) {
-        if (x > 0 && y > 0) {
-            this.S = Math.sqrt(x * x + y * y);
-            this.Add = Math.sqrt((x + a) * (x + a) + (y + b) * (y + b));
-            this.Sub = Math.sqrt((x - a) * (x - a) + (y - b) * (y - b));
-            this.Mult = Math.sqrt((x * c) * (x * c) + (y * c) * (y * c));
-        }
+    public void getVec() {
+        System.out.println(this.x + " " + this.y);
     }
 
-    public void add(double x, double y) {
-        this.Add = Math.sqrt((x + a) * (x + a) + (y + b) * (y + b));
+    public void setAdd (double a, double b) {
+        this.x += a;
+        this.y += b;
     }
-    public double getAll () {
-        return this.Add;
-        return this.Sub;
-        return this.Mult;
+    public void getAdd() {
+        System.out.println(this.x + " " + this.y);
+    }
+
+    public void setSub (double a, double b) {
+        this.x -= a;
+        this.y -= b;
+    }
+    public void getSub() {
+        System.out.println(this.x + " " + this.y);
+    }
+
+    public void setMult (double a) {
+        this.x *= a;
+        this.y *= a;
+    }
+    public void getMult() {
+        System.out.println(this.x + " " + this.y);
     }
 }
