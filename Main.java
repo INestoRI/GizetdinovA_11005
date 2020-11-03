335814cc77f6e229db6e64a52ab0ad15d4de771d
Albert Gizetdinov <albert160616@gmail.com>
2020-11-03 15:10:13 +0300

    Homework0611

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Vector2D vec = new Vector2D(3, 5);
         vec.printVector2D();
+
         vec.setAdd(4, 8);
         vec.printVector2D();
+
         vec.setSub(5, 9);
         vec.printVector2D();
+        
         vec.setMult(4);
         vec.printVector2D();
     }
--- Vector2D.java
+++ Vector2D.java
@@ -2,6 +2,7 @@ public class Vector2D {
     private double x;
     private double y;
 
+    //Task 1
     public Vector2D (double x, double y) {
         this.x = x;
         this.y = y;
@@ -11,16 +12,19 @@ public class Vector2D {
         System.out.println(this.x + " " + this.y);
     }
 
+    //Task 2
     public void setAdd (double x, double y) {
         this.x += x;
         this.y += y;
     }
 
+    //Task 3
     public void setSub (double x, double y) {
         this.x -= x;
         this.y -= y;
     }
 
+    //Task 4
     public void setMult (double a) {
         this.x *= a;
         this.y *= a;
