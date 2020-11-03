a42a0e11097f11510e610924207fba77dd0a8341
Albert Gizetdinov <albert160616@gmail.com>
2020-11-03 14:53:12 +0300

    Homework0611

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Vector2D vec = new Vector2D(3, 5);
-        vec.getVector2D();
+        vec.printVector2D();
         vec.setAdd(4, 8);
-        vec.getAdd();
+        vec.printVector2D();
         vec.setSub(5, 9);
-        vec.getSub();
+        vec.printVector2D();
         vec.setMult(4);
-        vec.getMult();
+        vec.printVector2D();
     }
 }
